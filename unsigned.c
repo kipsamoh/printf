9966826dca423c414fee947412ae335892e07813
kipsamoh <133492659+kipsamoh@users.noreply.github.com>
2023-07-25 16:36:25 +0300

    Update unsigned.c

--- unsigned.c
+++ unsigned.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * print_unsigned - Print a unsigned int
- * @list: Number to print
+ * print_unsigned - outputs non negative num
+ * @list: num input
  *
- * Return: Length of the number
+ * Return: num size
  **/
 int print_unsigned(va_list list)
 {
