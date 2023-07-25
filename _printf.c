397542998ea33ecb9ed79880ad1e160e4012c5e1
kipsamoh <133492659+kipsamoh@users.noreply.github.com>
2023-07-25 18:33:35 +0300

    _printf.c

--- _printf.c
+++ _printf.c
@@ -8,21 +8,21 @@
  **/
 int _printf(const char *format, ...)
 {
-	int size;
+	int m;
 	va_list args;
 
 	if (format == NULL)
 		return (-1);
 
-	size = _strlen(format);
-	if (size <= 0)
+	m = _strlen(format);
+	if (m <= 0)
 		return (0);
 
 	va_start(args, format);
-	size = handler(format, args);
+	m = handler(format, args);
 
 	_putchar(-1);
 	va_end(args);
 
-	return (size);
+	return (m);
 }
