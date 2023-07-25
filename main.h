6f9d9ab1363c2bcbb7fd735d068a26fa0cd86213
kipsamoh <kipsamoh@gmail.com>
2023-07-24 18:55:29 -0700

    update

--- main.h
+++ main.h
@@ -2,8 +2,8 @@
 #define MAIN_H
 
 #include <stdarg.h>
-#include <stdlib.h>
 #include <unistd.h>
+#include <stdlib.h>
  
 /**
  * struct output - struct for associated output functions.
@@ -49,7 +49,7 @@ int output_nupx(va_list args, char *bffr, unsigned int elem_bffr);
 int output_sint(va_list args, char *bffr, unsigned int elem_bffr);
 int (*obtain_output_functions(const char *ch, int element))(va_list, char *, unsigned int);
 int hpr_output_functions(const char *ch, int element);
-unsigned int bffr_handle(char *bffr, char c, unsigned int elem_bffr);
+unsigned int bffr_handle(char *bffr, char z, unsigned int elem_bffr);
 char *stack_octal_arr(char *binry, char *oct);
 char *stack_l_octal_arr(char *binry, char *oct);
 int output_bffr(char *bffr, unsigned int n_bffr);
