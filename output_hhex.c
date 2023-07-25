0dae9607799cf9d8656ad556cede55e16e3d45c5
kipsamoh <133492659+kipsamoh@users.noreply.github.com>
2023-07-25 18:43:15 +0300

    output_hhex.c

--- output_hhex.c
+++ output_hhex.c
@@ -12,14 +12,14 @@ char *string_to_upper(char *);
 int print_hexadecimal_upp(va_list list)
 {
 	char *p_buff;
-	int size;
+	int m;
 
 	p_buff = itoa(va_arg(list, unsigned int), 16);
 	p_buff = string_to_upper(p_buff);
 
-	size = print((p_buff != NULL) ? p_buff : "NULL");
+	m = print((p_buff != NULL) ? p_buff : "NULL");
 
-	return (size);
+	return (m);
 }
 
 /**
