09754b17b5dea1b3a3305854b4f002fce48b1cef
Daniel Chebet <133492659+kipsamoh@users.noreply.github.com>
2023-11-12 15:16:34 +0300

    generate-authors.sh

--- generate-authors.sh
+++ generate-authors.sh
@@ -16,6 +16,8 @@ set -x
 # Generate AUTHORS file, placing kipsamoh <kipsamoh@gmail.com> as the first contributor
 {
   printf "%-4s %-30s %-50s\n" "# This file lists all contributors to the repository." "" ""
+
+  
   git log --format='%aN <%aE>' | grep -v -E '^(kipsamoh <133492659\+kipsamoh@users\.noreply\.github\.com>|>)$' | LC_ALL=C.UTF-8 sort -uf | sed 's/ / /g'
 } > "${ROOTDIR}/printf/AUTHORS"
 
